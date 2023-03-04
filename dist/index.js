50c55587d67b2ef52a9cd4b8b496ca73a7052e5c
neeswebservices <neeswebservice@gmail.com>
2023-03-04 17:40:27 +0545

    ffmpeg

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,48 @@
 import ytpl from "ytpl";
-const passURL = "https://www.youtube.com/playlist?list=PLopcHtZ0hJF20vLxAKkCus_NTwwrkCfKy";
+import ytdl from "ytdl-core";
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import * as url from "url";
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const playListURL = "https://www.youtube.com/playlist?list=PLopcHtZ0hJF20vLxAKkCus_NTwwrkCfKy";
+// ffmpeg.setFfmpegPath(path.join(__dirname, "path", "to", "ffmpeg"));
 async function downloadPlaylist(playListURL, outputDir) {
+    const ids = [];
     const playListID = playListURL.split("list=")[1];
     const playlist = await ytpl(playListID);
     for (const video in playlist.items) {
         if (Object.prototype.hasOwnProperty.call(playlist.items, video)) {
             const element = playlist.items[video];
-            console.log(element);
+            ids.push(element === null || element === void 0 ? void 0 : element.url);
         }
     }
+    return new Promise((res, rej) => {
+        if (ids.length <= 0)
+            return rej("Invalid url | No videos found.");
+        return res(ids);
+    });
 }
-downloadPlaylist(passURL, "as");
+downloadPlaylist(playListURL).then((data) => downloadVideos(data));
+async function downloadVideos(urls) {
+    for (const url of urls) {
+        const stream = ytdl(url, {
+            quality: "highest",
+            filter: "audioandvideo",
+        });
+        const ffmpegProcess = ffmpeg();
+        console.log(ffmpegProcess);
+        ffmpegProcess
+            .input(stream)
+            .outputOption("-c:v copy")
+            .outputOption("-c:a copy")
+            .format("mp4")
+            .on("error", (err) => {
+            console.log(err);
+            console.log(`An error occurred ${err.message}`);
+        })
+            .on("end", () => {
+            console.log(`Finished downloading ${url}!`);
+        })
+            .pipe();
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,59 @@
 import ytpl from "ytpl";
 import ytdl from "ytdl-core";
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import * as url from "url";
+import ffmpegStatic from "ffmpeg-static";
 
-const passURL = "https://www.youtube.com/playlist?list=PLopcHtZ0hJF20vLxAKkCus_NTwwrkCfKy";
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-async function downloadPlaylist(playListURL: string, outputDir: string): Promise<void> {
+const playListURL = "https://www.youtube.com/playlist?list=PLopcHtZ0hJF20vLxAKkCus_NTwwrkCfKy";
+
+// ffmpeg.setFfmpegPath(path.join(__dirname, "path", "to", "ffmpeg"));
+
+async function downloadPlaylist(playListURL: string, outputDir?: string): Promise<string[]> {
+  const ids: string[] = [];
+
   const playListID = playListURL.split("list=")[1];
   const playlist = await ytpl(playListID);
 
   for (const video in playlist.items) {
     if (Object.prototype.hasOwnProperty.call(playlist.items, video)) {
       const element = playlist.items[video];
-      console.log(element);
+      ids.push(element?.url);
     }
   }
+
+  return new Promise((res, rej) => {
+    if (ids.length <= 0) return rej("Invalid url | No videos found.");
+
+    return res(ids);
+  });
 }
 
-downloadPlaylist(passURL, "as");
+downloadPlaylist(playListURL).then((data) => downloadVideos(data));
+
+async function downloadVideos(urls: string[]) {
+  for (const url of urls) {
+    const stream = ytdl(url, {
+      quality: "highest",
+      filter: "audioandvideo",
+    });
+
+    const ffmpegProcess = ffmpeg();
+    console.log(ffmpegProcess);
+    ffmpegProcess
+      .input(stream)
+      .outputOption("-c:v copy")
+      .outputOption("-c:a copy")
+      .format("mp4")
+      .on("error", (err) => {
+        console.log(err);
+        console.log(`An error occurred ${err.message}`);
+      })
+      .on("end", () => {
+        console.log(`Finished downloading ${url}!`);
+      })
+      .pipe();
+  }
+}
